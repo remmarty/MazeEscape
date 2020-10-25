cafe81d7a995c21839c2484f9075c9ebbec52f4c
remmarty <remi.martyniak@linux.pl>
2020-10-26 00:45:25 +0100

    first commit
    creating and starting up the game window

---
+++ MazeEscape/src/game/Launcher.java
@@ -0,0 +1,10 @@
+package game;
+import window.Window;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        new Window(1024, 768, "MazeEscape"); // create window
+    }
+}
---
+++ MazeEscape/src/window/Window.java
@@ -0,0 +1,39 @@
+package window;
+
+import javax.swing.*;
+import java.awt.*;
+
+    public class Window {
+
+        private Canvas canvas;
+        private JFrame frame;
+        private int width, height;
+        private String title;
+
+        public Window(int width, int height, String title) {
+            this.width = width;
+            this.height = height;
+            this.title = title;
+            Display();
+        }
+
+        private void Display() {       // method that manages our window and canvas
+
+            frame = new JFrame(title);
+            frame.setLocationRelativeTo(null);
+            frame.setSize(width, height);
+            frame.setTitle(title);
+            frame.setResizable(false);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+
+            canvas = new Canvas();
+            canvas.setPreferredSize(new Dimension(width, height));
+            canvas.setMaximumSize(new Dimension(width, height));
+            canvas.setMinimumSize(new Dimension(width, height));
+            canvas.setFocusable(false);
+            frame.add(canvas);
+            frame.pack();
+
+        }
+}
